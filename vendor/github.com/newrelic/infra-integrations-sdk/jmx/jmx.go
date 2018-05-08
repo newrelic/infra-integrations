a6ac4a9d903a5dd5b543191204513d67b1ea1d32
Juan Hernandez <jhernandez@newrelic.com>
2018-05-08 10:01:41 -0700

    Fix JMX races

--- vendor/github.com/newrelic/infra-integrations-sdk/jmx/jmx.go
+++ vendor/github.com/newrelic/infra-integrations-sdk/jmx/jmx.go
@@ -8,6 +8,7 @@ import (
 	"bufio"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -17,6 +18,7 @@ import (
 	"time"
 )
 
+var lock sync.Mutex
 var cmd *exec.Cmd
 var cancel context.CancelFunc
 var cmdOut io.ReadCloser
@@ -24,7 +26,11 @@ var cmdIn io.WriteCloser
 var cmdErr = make(chan error, 1)
 var done sync.WaitGroup
 
-var jmxCommand = "/usr/bin/nrjmx"
+var (
+	jmxCommand = "/usr/bin/nrjmx"
+	// ErrJmxCmdRunning error returned when trying to Open and nrjmx command is still running
+	ErrJmxCmdRunning = errors.New("JMX tool is already running")
+)
 
 const (
 	jmxLineBuffer = 4 * 1024 * 1024 // Max 4MB per line. If single lines are outputting more JSON than that, we likely need smaller-scoped JMX queries
@@ -49,8 +55,11 @@ func getCommand(hostname, port, username, password string) []string {
 
 // Open will start the nrjmx command with the provided connection parameters.
 func Open(hostname, port, username, password string) error {
+	lock.Lock()
+	defer lock.Unlock()
+
 	if cmd != nil {
-		return fmt.Errorf("JMX tool is already running with PID: %d", cmd.Process.Pid)
+		return ErrJmxCmdRunning
 	}
 
 	// Drain error channel to prevent showing past errors
@@ -66,8 +75,6 @@ func Open(hostname, port, username, password string) error {
 	cliCommand := getCommand(hostname, port, username, password)
 
 	ctx, cancel = context.WithCancel(context.Background())
-	// Avoid stupid errors/warnings b/c cancel is not used in this method
-	_ = cancel
 
 	cmd = exec.CommandContext(ctx, cliCommand[0], cliCommand[1:]...)
 
@@ -86,6 +93,9 @@ func Open(hostname, port, username, password string) error {
 			cmdErr <- fmt.Errorf("JMX tool exited with error: %s", err)
 		}
 		done.Done()
+
+		lock.Lock()
+		defer lock.Unlock()
 		cmd = nil
 	}()
 
@@ -95,29 +105,43 @@ func Open(hostname, port, username, password string) error {
 // Close will finish the underlying nrjmx application by closing its standard
 // input and canceling the execution afterwards to clean-up.
 func Close() {
+	lock.Lock()
+	defer lock.Unlock()
+
 	if cmd == nil {
 		return
 	}
 
-	cmdIn.Close()
 	cancel()
+	_ = cmdIn.Close()
 	done.Wait()
 }
 
-func doQuery(out chan []byte, errorChan chan error, queryString []byte) {
-	cmdIn.Write(queryString)
+func doQuery(ctx context.Context, out chan []byte, errorChan chan error, queryString []byte) {
+	lock.Lock()
+	if _, err := cmdIn.Write(queryString); err != nil {
+		lock.Unlock()
+		errorChan <- fmt.Errorf("writing query string: %s", err.Error())
+		return
+	}
 
 	scanner := bufio.NewScanner(cmdOut)
 	scanner.Buffer([]byte{}, jmxLineBuffer) // Override default buffer to increase buffer size
+	lock.Unlock()
 
 	if scanner.Scan() {
-		out <- scanner.Bytes()
+		select {
+		case <-ctx.Done():
+			return
+		case out <- scanner.Bytes():
+		default:
+		}
 	} else {
 		if err := scanner.Err(); err != nil {
-			errorChan <- fmt.Errorf("Error reading output from JMX tool: %v", err)
+			errorChan <- fmt.Errorf("error reading output from JMX tool: %v", err)
 		} else {
 			// If scanner.Scan() returns false but err is also nil, it hit EOF. We consider that a problem, so we should return an error.
-			errorChan <- fmt.Errorf("Got an EOF while reading JMX tool output")
+			errorChan <- fmt.Errorf("got an EOF while reading JMX tool output")
 		}
 	}
 }
@@ -125,21 +149,24 @@ func doQuery(out chan []byte, errorChan chan error, queryString []byte) {
 // Query executes JMX query against nrjmx tool waiting up to timeout (in milliseconds)
 // and returns a map with the result.
 func Query(objectPattern string, timeout int) (map[string]interface{}, error) {
+	ctx, cancelFn := context.WithCancel(context.Background())
+
 	result := make(map[string]interface{})
-	pipe := make(chan []byte)
+	lineCh := make(chan []byte, jmxLineBuffer*2)
 	queryErrors := make(chan error)
 	outTimeout := time.Duration(timeout) * time.Millisecond
 	// Send the query async to the underlying process so we can timeout it
-	go doQuery(pipe, queryErrors, []byte(fmt.Sprintf("%s\n", objectPattern)))
+	go doQuery(ctx, lineCh, queryErrors, []byte(fmt.Sprintf("%s\n", objectPattern)))
 
 	select {
-	case line := <-pipe:
+	case line := <-lineCh:
 		if line == nil {
+			cancelFn()
 			Close()
-			return nil, fmt.Errorf("Got empty result for query: %s", objectPattern)
+			return nil, fmt.Errorf("got empty result for query: %s", objectPattern)
 		}
 		if err := json.Unmarshal(line, &result); err != nil {
-			return nil, fmt.Errorf("Invalid return value for query: %s, %s", objectPattern, err)
+			return nil, fmt.Errorf("invalid return value for query: %s, %s", objectPattern, err)
 		}
 	case err := <-cmdErr: // Will receive an error if the nrjmx tool exited prematurely
 		return nil, err
@@ -147,8 +174,9 @@ func Query(objectPattern string, timeout int) (map[string]interface{}, error) {
 		return nil, err
 	case <-time.After(outTimeout):
 		// In case of timeout, we want to close the command to avoid mixing up results coming up latter
+		cancelFn()
 		Close()
-		return nil, fmt.Errorf("Timeout while waiting for query: %s", objectPattern)
+		return nil, fmt.Errorf("timeout while waiting for query: %s", objectPattern)
 	}
 	return result, nil
 }
