966f58d24b49e0664d8d4b0ea146a8741e83fa59
Mario Macias <mmacias@newrelic.com>
2018-08-01 14:46:12 +0200

    Changing RPM and DEB to be just meta packages

--- Makefile-package.mk
+++ Makefile-package.mk
@@ -1,8 +1,11 @@
+DEP_APACHE         = "nri-apache"
+DEP_CASSANDRA      = "nri-cassandra"
+DEP_MYSQL          = "nri-mysql"
+DEP_NGINX          = "nri-nginx"
+DEP_REDIS          = "nri-redis"
 PACKAGE_TYPES     ?= deb rpm
 PROJECT_NAME       = newrelic-infra-integrations
 BINS_PREFIX        = nr
-BINS_DIR           = $(TARGET_DIR)/bin/linux_amd64
-SOURCE_DIR         = $(TARGET_DIR)/source
 PACKAGES_DIR       = $(TARGET_DIR)/packages
 VERSION           ?= 0.0.0
 RELEASE           ?= dev
@@ -11,54 +14,36 @@ VENDOR             = "New Relic, Inc."
 PACKAGER           = "New Relic Infrastructure Team <infrastructure-eng@newrelic.com>"
 PACKAGE_URL        = "https://www.newrelic.com/infrastructure"
 SUMMARY            = "New Relic Infrastructure Integrations"
-DESCRIPTION        = "New Relic Infrastructure Integrations extend the core New Relic\nInfrastructure agent's capabilities to allow you to collect metric and\nlive state data from your infrastructure components such as MySQL,\nNGINX and Cassandra."
-FPM_COMMON_OPTIONS = --verbose -C $(SOURCE_DIR) -s dir -n $(PROJECT_NAME) -v $(VERSION) --iteration $(RELEASE) --prefix "" --license $(LICENSE) --vendor $(VENDOR) -m $(PACKAGER) --url $(PACKAGE_URL) --config-files /etc/newrelic-infra/ --description "$$(printf $(DESCRIPTION))" --depends "newrelic-infra >= 1.0.726" --depends "nrjmx"
+
+define DESCRIPTION
+New Relic Infrastructure Integrations extend the core New Relic\n\
+Infrastructure agent\'s capabilities to allow you to collect metric and\n\
+live state data from your infrastructure components such as MySQL,\n\
+NGINX and Cassandra.\n\
+\n\
+This is a meta-package that specifies dependencies to the\n\
+aforementioned integrations, and installs all of them. You may prefer\n\
+to install them individually, selecting only those that you need.
+endef
+
+FPM_COMMON_OPTIONS = --verbose -C empty -s dir -n $(PROJECT_NAME) -v $(VERSION) --iteration $(RELEASE)\
+ --prefix "" --license $(LICENSE) --vendor $(VENDOR) -m $(PACKAGER) --url $(PACKAGE_URL)\
+ --description "$$(printf $(DESCRIPTION))"\
+ --depends $(DEP_APACHE) --depends $(DEP_CASSANDRA) --depends $(DEP_MYSQL) --depends $(DEP_NGINX) --depends $(DEP_REDIS)
+
 FPM_DEB_OPTIONS    = -t deb -p $(PACKAGES_DIR)/deb/ --replaces "newrelic-infra-integrations-beta (<= 1.0.0)"
 FPM_RPM_OPTIONS    = -t rpm -p $(PACKAGES_DIR)/rpm/ --epoch 0 --rpm-summary $(SUMMARY) --replaces "newrelic-infra-integrations-beta <= 1.0.0"
 
-package: create-bins prep-pkg-env $(PACKAGE_TYPES)
+package: $(PACKAGE_TYPES)
 
-create-bins:
-	@for I in $(INTS); do \
-		if [ $$I != "example" ]; then \
-			PACKAGE=$$(go list ./integrations/$$I/... 2>&1) ;\
-			if echo $$PACKAGE | grep -Eq ".*matched\ no\ packages$$"; then \
-				echo "=== Main === [ create-bins ]: no Go files found for $$I. Skipping." ;\
-			else \
-				echo "=== Main === [ create-bins ]: creating binary for $$I..." ;\
-				go build -v -ldflags '-X main.buildVersion=$(VERSION)' -o $(BINS_DIR)/$(BINS_PREFIX)-$$I $$PACKAGE || exit 1 ;\
-			fi ;\
-		fi ;\
-	done
-	@echo ""
-
-prep-pkg-env:
-	@if [ ! -d $(BINS_DIR) ]; then \
-		echo "=== Main === [ prep-pkg-env ]: no built binaries found. Run 'make create-bins'" ;\
-		exit 1 ;\
-	fi
-	@echo "=== Main === [ prep-pkg-env ]: preparing a clean packaging environment..."
-	@rm -rf $(SOURCE_DIR)
-	@mkdir -p $(SOURCE_DIR)/var/db/newrelic-infra/newrelic-integrations/bin $(SOURCE_DIR)/etc/newrelic-infra/integrations.d
-	@echo "=== Main === [ prep-pkg-env ]: adding built binaries and configuration and definition files..."
-	@for BIN in $$(ls $(BINS_DIR)); do \
-		cp $(BINS_DIR)/$$BIN $(SOURCE_DIR)/var/db/newrelic-infra/newrelic-integrations/bin ;\
-		chmod 755 $(SOURCE_DIR)/var/db/newrelic-infra/newrelic-integrations/bin/* ;\
-		cp $(INTEGRATIONS_DIR)/$${BIN#$(BINS_PREFIX)-}/*.yml $(SOURCE_DIR)/var/db/newrelic-infra/newrelic-integrations/ ;\
-		chmod 644 $(SOURCE_DIR)/var/db/newrelic-infra/newrelic-integrations/*.yml ;\
-		cp $(INTEGRATIONS_DIR)/$${BIN#$(BINS_PREFIX)-}/*.sample $(SOURCE_DIR)/etc/newrelic-infra/integrations.d/ ;\
-		chmod 644 $(SOURCE_DIR)/etc/newrelic-infra/integrations.d/*.sample ;\
-	done
-	@echo ""
-
-deb: prep-pkg-env
+deb:
 	@echo "=== Main === [ deb ]: building DEB package..."
 	@mkdir -p $(PACKAGES_DIR)/deb
 	@fpm $(FPM_COMMON_OPTIONS) $(FPM_DEB_OPTIONS) .
 
-rpm: prep-pkg-env
+rpm:
 	@echo "=== Main === [ rpm ]: building RPM package..."
 	@mkdir -p $(PACKAGES_DIR)/rpm
 	@fpm $(FPM_COMMON_OPTIONS) $(FPM_RPM_OPTIONS) .
 
-.PHONY: package create-bins prep-pkg-env $(PACKAGE_TYPES)
+.PHONY: package $(PACKAGE_TYPES)
